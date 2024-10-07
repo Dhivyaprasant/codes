733c83dde820f5f263bd9bb3569e052abc90e25b
Dhivyaprasanth <138613489+Dhivyaprasant@users.noreply.github.com>
2024-10-07 19:46:10 +0530

    Update reverse number.py

--- reverse number.py
+++ reverse number.py
@@ -5,3 +5,10 @@ while(number>0):
     reverse_no=(reverse_no*10)+remainder
     number = number//10
     print("the reversed number:",reverse_no)
+
+
+# other way to solve
+
+number=input("enter the number:")
+    reversed_no= (number[::-1])
+    print(reversed_no)
